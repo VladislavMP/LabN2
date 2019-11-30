e450c3aa91435e5ef3ac01082f64c8b39712c016
VladislavMP <vladislav.mozheiko321@gmail.com>
2019-11-30 22:13:06 +0300

    Создание каркаса приложения

---
+++ src/lab2/varb4/MainFrame.java
@@ -0,0 +1,24 @@
+package lab2.varb4;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+
+public class MainFrame extends JFrame {
+}
