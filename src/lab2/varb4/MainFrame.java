b7bde9cad74c851e4438a23bf296dc8b35cd6064
VladislavMP <vladislav.mozheiko321@gmail.com>
2019-11-30 22:39:46 +0300

    Добавление поля textFieldZ Реализация методов-помощников вычисления значения функций

--- src/lab2/varb4/MainFrame.java
+++ src/lab2/varb4/MainFrame.java
@@ -7,6 +7,7 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.MathContext;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.ButtonGroup;
@@ -25,12 +26,23 @@ public class MainFrame extends JFrame {
     private static final int WIDTH = 400;
     private static final int HEIGHT = 320;
 
-    // Текстовые поля для считывания значений переменных X и Y
+    // Текстовые поля для считывания значений переменных
     private JTextField textFieldX;
     private JTextField textFieldY;
+    private JTextField textFieldZ;
 
     private JTextField textFieldResult;
     private ButtonGroup radioButtons = new ButtonGroup();
     private Box hboxFormulaType = Box.createHorizontalBox();
     private int formulaId = 1;
+
+    // Формула No1 для рассчѐта
+    public Double calculate1(Double x, Double y, Double z) {
+        return Math.sin(Math.sin(y) + Math.exp(Math.cos(y)) + z * z)
+                * Math.pow(x * x + Math.sin(z) + Math.exp(Math.cos(z)), 1/4);
+    }
+    // Формула No2 для рассчѐта
+    public Double calculate2(Double x, Double y, Double z) {
+        return Math.pow(x, x)/(Math.sqrt(y*y*y + 1) + Math.log(z));
+    }
 }
